604f4c7a16d753de60271707afdb3a80b79347b1
tomi-trost <tomi.trost02@gmail.com>
2025-03-05 19:30:16 +0100

    Added the worker package with the binary implementation of timeslot matrix, used in the Worker class

---
+++ app/models/timeslot/timeslot_matrix_binary.py
@@ -0,0 +1,29 @@
+﻿import numpy as np
+
+from models.timeslot.timeslot_matrix import TimeSlotMatrix
+from models.timeslot.timeslot import TimeSlot
+
+class TimeSlotMatrixBinary(TimeSlotMatrix):
+
+    def __init__(self, xdim: int , ydim):
+        self.matrix = np.zeros((xdim, ydim))
+
+    def put(self, timeslot: TimeSlot):
+        """Lables a timeslot as available"""
+        ts_index = self._getMatrixIndex(timeslot)
+        self.matrix[*ts_index] = 1
+
+    def free(self, timeslot: TimeSlot):
+        """Lables a timeslot as unavailable"""
+        ts_index = self._getMatrixIndex(timeslot)
+        self.matrix[*ts_index] = 0
+
+    def count(self) -> int:
+        """Count the number of slots that are labled as available"""
+        return self.matrix.sum()
+    
+    def is_active(self, timeslot: TimeSlot) -> bool:
+        """Returns the state of the timeslot"""
+        ts_index = self._getMatrixIndex(timeslot)
+        return bool(self.matrix[*ts_index])
+
---
+++ app/models/worker/worker.py
@@ -0,0 +1,39 @@
+﻿import copy
+
+from models.timeslot.timeslot import TimeSlot
+from models.timeslot.timeslot_matrix_binary import TimeSlotMatrixBinary
+
+class Worker:
+
+    def __init__(
+        self,
+        availability: TimeSlotMatrixBinary,
+        assigned: TimeSlotMatrixBinary
+    ) -> None:
+        self.availability = availability
+        self.assigned = assigned
+    
+    @property
+    def cnt(self) -> int:
+        """Computed property that gets count from assigned matrix"""
+        return self.assigned.count()
+    
+    def clone(self) -> 'Worker':
+        """Returns a clone of the Worker object"""
+        return copy.deepcopy(self)
+    
+    def assign(self, timeslot: TimeSlot) -> bool:
+        """Assigns a timeslot to the Worker"""
+        if (not self.availability.is_active()):
+            raise Exception("The Worker isn't available for this timeslot.")
+        self.assigned.put(timeslot)
+        self.availability.free(timeslot)
+
+    def free(self, timeslot: TimeSlot) -> None:
+        """Frees the Worker from a timeslot"""
+        if (not self.assign.is_active()):
+            raise Exception("The Worker is not assigned to this timeslot.")
+        self.assigned.free(timeslot)
+        self.availability.put(timeslot)
+        
+

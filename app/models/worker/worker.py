70438f3bad2c064f6e3134eaaf3a51770b36b7d1
tomi-trost <tomi.trost02@gmail.com>
2025-03-06 19:59:51 +0100

    Added WorerPool class and added is_available and AvailabilityRange functionality to the Worker class

--- app/models/worker/worker.py
+++ app/models/worker/worker.py
@@ -5,13 +5,21 @@ from models.timeslot.timeslot_matrix_binary import TimeSlotMatrixBinary
 
 class Worker:
 
+    class AvailabilityRange:
+        
+        def __init__(self, min: int, max: int):
+            self.min = min
+            self.max = max
+
     def __init__(
         self,
         availability: TimeSlotMatrixBinary,
-        assigned: TimeSlotMatrixBinary
+        assigned: TimeSlotMatrixBinary,
+        availability_range: 'Worker.AvailabilityRange'
     ) -> None:
         self.availability = availability
         self.assigned = assigned
+        self.availability_range = availability_range
     
     @property
     def cnt(self) -> int:
@@ -27,8 +35,7 @@ class Worker:
         if (not self.availability.is_active(timeslot)):
             raise Exception("The Worker isn't available for this timeslot.")
         self.assigned.put(timeslot)
-        self.availability.free(timeslot)
-        
+        self.availability.free(timeslot) 
 
     def free(self, timeslot: TimeSlot) -> None:
         """Frees the Worker from a timeslot"""
@@ -36,5 +43,26 @@ class Worker:
             raise Exception("The Worker is not assigned to this timeslot.")
         self.assigned.free(timeslot)
         self.availability.put(timeslot)
+
+    def is_available(self, timeslot: TimeSlot) -> bool:
+        """Checks if Worker is available for a timeslot"""
+        return self.availability.is_active(timeslot)
         
+
+class WorkerPool:
+
+    def __init__(self):
+        self.workers: set[Worker] = {}
+    
+    def get_workers(self) -> set[Worker]:
+        """Returns a set of all of the Workers in the WorkerPool"""
+        return self.workers
+
+    def get_available_workers(self, timeslot: TimeSlot) -> set[Worker]:
+        """Returns a set of workers, that are available on some timeslot"""
+        return {worker for worker in self.workers if worker.is_available(timeslot)}
+
+    def push_worker(self, worker: Worker) -> None:
+        """Adds a worker to the WorkerPool set"""
+        self.workers.add(worker)
 

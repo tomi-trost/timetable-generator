deaabe71573cd8f6e81ea1456a5d63ed815964bf
tomi-trost <tomi.trost02@gmail.com>
2025-03-12 10:52:49 +0100

    Worker Pool class moved to a seperate file

--- app/models/worker/worker.py
+++ app/models/worker/worker.py
@@ -82,22 +82,4 @@ class Worker:
     def is_available(self, timeslot: TimeSlot) -> bool:
         """Checks if Worker is available for a timeslot"""
         return self.availability.is_active(timeslot)
-        
-
-class WorkerPool:
-
-    def __init__(self):
-        self.workers: set[Worker] = {}
     
-    def get_workers(self) -> set[Worker]:
-        """Returns a set of all of the Workers in the WorkerPool"""
-        return self.workers
-
-    def get_available_workers(self, timeslot: TimeSlot) -> set[Worker]:
-        """Returns a set of workers, that are available on some timeslot"""
-        return {worker for worker in self.workers if worker.is_available(timeslot)}
-
-    def push_worker(self, worker: Worker) -> None:
-        """Adds a worker to the WorkerPool set"""
-        self.workers.add(worker)
-
---
+++ app/models/worker/worker_pool.py
@@ -0,0 +1,20 @@
+﻿from models.worker.worker import Worker
+from models.timeslot.timeslot import TimeSlot
+
+class WorkerPool:
+
+    def __init__(self):
+        self.workers: set[Worker] = {}
+    
+    def get_workers(self) -> set[Worker]:
+        """Returns a set of all of the Workers in the WorkerPool"""
+        return self.workers
+
+    def get_available_workers(self, timeslot: TimeSlot) -> set[Worker]:
+        """Returns a set of workers, that are available on some timeslot"""
+        return {worker for worker in self.workers if worker.is_available(timeslot)}
+
+    def push_worker(self, worker: Worker) -> None:
+        """Adds a worker to the WorkerPool set"""
+        self.workers.add(worker)
+

2434bdd71ab6c7ed8abd9dc8791fc8b53d4ade27
tomi-trost <tomi.trost02@gmail.com>
2025-03-14 08:46:22 +0100

    Overloaded the constructor to be optionaly envoked using an existing matrix

--- app/models/timeslot_matrix/timeslot_matrix_resources.py
+++ app/models/timeslot_matrix/timeslot_matrix_resources.py
@@ -18,19 +18,32 @@ class TimeSlotMatrixResources(TimeSlotMatrix):
         shifts (int): The number of shifts per day.
     """
 
-    def __init__(self, days: int , shifts: int):
+    def __init__(self, days: int = None, shifts: int = None, matrix: np.ndarray = None):
         """
-        Initializes a TimeSlotMatrixBinary object.
+        Initializes a TimeSlotMatrixResources object.
 
         Args:
-            days (int): The number of days.
-            shifts (int): The number of shifts per day.
+            days (int, optional): The number of days.
+            shifts (int, optional): The number of shifts per day.
+            matrix (np.ndarray, optional): A predefined matrix to use.
 
-        The matrix is initialized as a 2D NumPy array filled with zeros.
+        If a matrix is provided, its dimensions are used for days and shifts.
+        Otherwise, a zero-initialized matrix is created based on the days and shifts provided.
         """
-        self.matrix = np.zeros((days, shifts))
-        self.days = days
-        self.shifts = shifts
+        if matrix is not None:
+            if not isinstance(matrix, np.ndarray):
+                raise ValueError("Matrix must be a NumPy array.")
+            if matrix.ndim != 2:
+                raise ValueError("Matrix must be a 2D array.")
+            
+            self.matrix = matrix
+            self.days, self.shifts = matrix.shape  # Extract dimensions from the matrix
+        elif days is not None and shifts is not None:
+            self.matrix = np.zeros((days, shifts))
+            self.days = days
+            self.shifts = shifts
+        else:
+            raise ValueError("Either provide days and shifts, or a matrix.")
 
     def increment(self, timeslot: TimeSlot):
         """Increments the value of a resource for a timeslot"""

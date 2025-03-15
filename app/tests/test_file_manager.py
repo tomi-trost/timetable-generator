0b67fbd4a54c34d98e64e270f636345097239a1f
tomi-trost <tomi.trost02@gmail.com>
2025-03-15 19:16:20 +0100

    Created unit tests for FileManager class

---
+++ app/tests/test_file_manager.py
@@ -0,0 +1,10 @@
+﻿from app.models.file_manager.file_manager import FileManager
+from app.models.timetable.timetable import TimeTable
+
+def test_file_manager():
+
+    timetable: TimeTable = FileManager.read_timetable("app/data/timetable.json")
+
+    assert timetable.is_solvable() == True
+    shortage = timetable.get_shortage()
+    assert shortage.sum() == 0

3c8b1ae04f37389e806017812ac2d9f394a9432a
tomi-trost <tomi.trost02@gmail.com>
2025-03-17 18:25:01 +0100

    Added a template for classes needed for a_star algoritm implementation and added a worker heuristic method to the heuristics class

---
+++ app/models/a_star/heuristics.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+
+from app.models.worker.worker import Worker
+from app.models.timeslot_matrix.timeslot_matrix_resources import TimeSlotMatrixResources
+
+class Heuristics:
+
+        
+    def worker_heuristic(worker: Worker, worker_supply: TimeSlotMatrixResources, worker_demand: TimeSlotMatrixResources) -> int:
+        """Returns a heuristic value for a worker"""
+        worker_assigned_count = worker.assigned.count()
+
+        # Computes the probability of a worker being assigned to a timeslot
+        # Formula: Takes into acount already assigned timeslots and uses worker.availability.count() for normalization
+        assign_probability = (worker_assigned_count + np.sum((worker_demand.matrix * worker.availability.matrix) / worker_supply.matrix)) / worker.availability.count() 
+        # Computes the users rating from maximum rating
+        worker_rating = worker.rating / Worker.max_rating
+        # Computes satisfaction factor based on the prefered number of timeslots it wants to be assigned
+        worker_satisfaction = worker_assigned_count / worker.availability_range.min
+
+        heuristics = [1/worker_rating, assign_probability, worker_satisfaction]
+        weights = [1/2, 1/4, 1/4]
+        return sum([heuristic * weight for heuristic, weight in zip(heuristics, weights)])
